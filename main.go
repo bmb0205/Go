3405b6319dc6f0e7cf2d4e89550a843164f81126
bmb0205 <bburciag@uncc.edu>
2016-05-17 16:30:49 -0400

    need to display time pairs and do a check if timerName exists

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ import (
 )
 
 type StatusStruct struct {
-	TimerName string `json:"timername"`
-	// TotalTime time.Time   `json:"totaltime"`
-	// TimePairs []time.Time `json:"timepairs"`
+	TimerName string        `json:"timername"`
+	TotalTime time.Duration `json:"totaltime"`
+	// TimePairs map[string]time.Time `json:"timepairs"`
 }
 
 type StartStruct struct {
@@ -33,13 +33,26 @@ Returns JSON response including total accumulated time for the specified
 timer and and all start/stop pairs that contributed to it.
 */
 func status(w http.ResponseWriter, r *http.Request, myMap map[string][]map[string]time.Time) {
+	fmt.Println(" ")
 	fmt.Println("Endpoint hit: status")
 
 	timerName := r.URL.Query().Get("timerName")
-	fmt.Println("lol", timerName)
+
+	var totalTime time.Duration
+
+	for _, valueMap := range myMap {
+		var tot time.Duration
+		for i := range valueMap {
+			start := valueMap[i]["startTime"]
+			stop := valueMap[i]["stopTime"]
+			tot += stop.Sub(start)
+		}
+		totalTime = (tot / time.Second) //(stop.Sub(start) / time.Second)
+
+	}
 	timer := StatusStruct{
 		timerName,
-		// totalTime,
+		totalTime,
 		// pairs of start/stop times
 	}
 
@@ -53,24 +66,11 @@ func status(w http.ResponseWriter, r *http.Request, myMap map[string][]map[strin
 	// Check request method type, write header and handle byte version of JSON data b
 	if r.Method == "GET" {
 		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
-		fmt.Println(err)
-		bytes := []byte(b)
-		fmt.Println(string(bytes))
-
 		w.Write(b)
 	} else {
 		fmt.Println("Should be using a GET request...")
 	}
 
-	// _, ok := myMap[timerName]
-	// fmt.Println(ok)
-	for k, v := range myMap {
-		fmt.Println("K: ", k)
-		fmt.Println("V: ", v)
-		fmt.Println(" ")
-		w.Write([]byte(k))
-	}
-	// fmt.Println(myMap)
 }
 
 /*
@@ -79,17 +79,13 @@ Returns JSON response including the total tracked time for the given timer and
 a created boolean indicating if the timer is new.
 */
 func start(w http.ResponseWriter, r *http.Request, myMap map[string][]map[string]time.Time) {
+	fmt.Println(" ")
 	fmt.Println("Endpoint hit: start")
-	// start := time.Now()
-	// var m map[string]time.Time
-	// m = make(map[string]time.Time)
+
 	// need map of timer name to start and stop values
 	// accept post request of timer name and time stamp start time
 	// check if timer name exists in map, set boolean to true or false
 	// return total tracked time which should be current total time
-
-	// s := New()
-	// s.start()
 
 	r.Header.Add("Content-Type", "application/json; charset=UTF-8")
 	body, err := ioutil.ReadAll(r.Body)
@@ -107,22 +103,11 @@ func start(w http.ResponseWriter, r *http.Request, myMap map[string][]map[string
 		var s StartStruct
 		json.Unmarshal(bytes, &s)
 
-		// s.StartTime is time stamp sent from js ajax post request
-		// might be 2 sec delay from s.StartTime and startTime (time.Now())
-		// true start time is s.StartTime
-
 		subMap := map[string]time.Time{
 			"startTime": startTime,
 			"stopTime":  time.Now(), // placeholder for future real stop time
 		}
-		// fmt.Println(startTime)
-		// fmt.Println(s.StartTime)
-		// fmt.Println(startTime.Sub(s.StartTime))
-		// fmt.Println(s.StartTime)
 		myMap[s.TimerName] = append(myMap[s.TimerName], subMap)
-		// fmt.Println(myMap)
-
-		// fmt.Println("endddddddd")
 
 	} else {
 		fmt.Errorf("should be using a POST request...")
@@ -134,6 +119,7 @@ JSON stop endpoint accepts timer name and time stamp via AJAX POST request.
 Returns JSON response including the total tracked time for the given timer.
 */
 func stop(w http.ResponseWriter, r *http.Request, myMap map[string][]map[string]time.Time) {
+	fmt.Println(" ")
 	fmt.Println("Endpoint hit: stop")
 
 	r.Header.Add("Content-Type", "application/json; charset=UTF-8")
@@ -146,33 +132,12 @@ func stop(w http.ResponseWriter, r *http.Request, myMap map[string][]map[string]
 	// writes request body back as response...change this to give back total time
 	if r.Method == "POST" {
 		stopTime := time.Now() // this needs the delay subtracted from it for a true value
-
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.Write(body)
 		bytes := []byte(body)
 		var s StopStruct
 		json.Unmarshal(bytes, &s)
 		myMap[s.TimerName][len(myMap[s.TimerName])-1]["stopTime"] = stopTime
-
-		// 'stop' pressed by client...example 2 sec delay...stop timestamp (time.Now()) called here
-		// delay added 2 sec to total time so a false reading is given
-		// must subtract delay from time.Now()
-
-		for name, valueMap := range myMap {
-			fmt.Println(name)
-			for i := range valueMap {
-				start := valueMap[i]["startTime"]
-				stop := valueMap[i]["stopTime"]
-				fmt.Println(start)
-				// fmt.Println(stop  s.StopTime)
-				fmt.Println(stop)
-				// fmt.Println(s.StopTime)
-				// fmt.Println(stop.Sub(s.StopTime))
-				fmt.Println(stop.Sub(start)) // difference in js stop timestamp and time.Now() stop timesnamp
-				fmt.Println(" ")
-			}
-		}
-
 	} else {
 		fmt.Errorf("should be using a POST request...")
 	}
